138927eee376a67ed4dd0c6b69be1e7b928c3914
esrasen9 <esrasen860@gmail.com>
2021-03-22 04:20:39 +0300

    22 Mart

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+        public IResult Add(CarImage carImage)
+        {
+            _carImageDal.Add(carImage);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            _carImageDal.Delete(carImage);
+            return new SuccessResult();
+        }
+
+
+        public IResult Update(CarImage carImage)
+        {
+            _carImageDal.Update(carImage);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll());
+        }
+
+        public IDataResult<CarImage> GetById(int carImageId)
+        {
+            return new SuccessDataResult<CarImage>(_carImageDal.Get(c => c.Id == carImageId));
+        }
+
+        public IDataResult<List<CarImage>> GetByCarId(int carId)
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(c => c.CarId == carId));
+        }
+    }
+}
---
+++ WebAPI/Controllers/CarImagesController.cs
@@ -0,0 +1,89 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarImagesController : ControllerBase
+    {
+        ICarImageService _carImageService;
+
+        public CarImagesController(ICarImageService carImageService)
+        {
+            _carImageService = carImageService;
+        }
+
+        [HttpGet("add")]
+        public IActionResult Add(CarImage carImage)
+        {
+            var result = _carImageService.Add(carImage);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(CarImage carImage)
+        {
+            var result = _carImageService.Delete(carImage);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update(CarImage carImage)
+        {
+            var result = _carImageService.Update(carImage);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _carImageService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int carImageId)
+        {
+            var result = _carImageService.GetById(carImageId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getimagesbycarid")]
+        public IActionResult GetImagesByCarId(int carId)
+        {
+            var result = _carImageService.GetByCarId(carId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
